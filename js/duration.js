81b2a6674ab81184bead50df400cd230f69695e1
Tebogo <tebogo3280359024@outlook.com>
2025-06-12 10:00:00 +0800

    Site updated: 2025-06-12 09:59:57

--- js/duration.js
+++ js/duration.js
@@ -1,6 +1,6 @@
 !(function() {
   /** 计时起始时间，自行修改 **/
-  var start = new Date("2025/06/10 00:00:00");
+  var start = new Date("2025/06/7 00:00:00");
 
   function update() {
     var now = new Date();
